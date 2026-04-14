4508cb4b643855a298024faafb11da4400d550fa
Priya <43725307+priyakalashi@users.noreply.github.com>
2026-04-14 18:26:04 +0530

    Fix import statement for matplotlib in FirstWebAppCode.py

--- FirstWebAppCode.py
+++ FirstWebAppCode.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import math
-import matplotlib as plt
+import matplotlib.plt as plt
 
 st.title ("Mortgage Repayment Calculator")
 st.write("### Input Data")
