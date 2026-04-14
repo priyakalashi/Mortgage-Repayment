b8a890cd6dad2ffcb9901c7795ba6b567f4c03bd
Priya <43725307+priyakalashi@users.noreply.github.com>
2026-04-14 18:27:00 +0530

    Update FirstWebAppCode.py

--- FirstWebAppCode.py
+++ FirstWebAppCode.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import math
-import matplotlib.plt as plt
+import matplotlib.pyplot as plt
 
 st.title ("Mortgage Repayment Calculator")
 st.write("### Input Data")
